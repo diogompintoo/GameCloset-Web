94930b7a00ba754ef634cb180e8419c33cc3d995
diogompintoo <diogomartinsp1992@gmail.com>
2026-05-12 01:26:04 +0100

    start gameClosetWebApp

---
+++ src/main/java/com/gamecloset/gamecloset_web/GameclosetWebApplication.java
@@ -0,0 +1,13 @@
+package com.gamecloset.gamecloset_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameclosetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameclosetWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamecloset/gamecloset_web/GameclosetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamecloset.gamecloset_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameclosetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
